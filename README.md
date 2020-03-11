31ca05522635622540c4fc355b478c02311e4ddf
Seanbot2000 <snoyes@pivotal.io>
2020-03-11 12:02:37 -0500

    Updating to allow vsphere and automate credhub properties

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # deploy-bucc
 
-Make sure to grab a release and not master
+Make sure to grab a release and not master from https://github.com/starkandwayne/bucc
 
 For GCP: bucc up --cpi gcp --concourse-lb
+- Add a tag so we can create some firewall rules around it.  I've used "bucc" in the sample vars file.
 
-Add a tag so we can create some firewall rules around it.  I've used "bucc" in the sample vars file.
+Additional installation:
+- jq
+- yyt
+- certbot
+- docker
+- yaml-patch
+- minio (for on-prem - create cert, run as https)
+	sudo certbot certonly --dns-cloudflare \
+       --dns-cloudflare-propagation-seconds 60  \
+       --dns-cloudflare-credentials ./cloudflare.ini \
+       --preferred-challenges dns-01 \
+       -d <minio.domain> \
+       --agree-tos \
+       -m <email>
+    sudo docker run -p 443:443 --name minio1 -e "MINIO_ACCESS_KEY=minio" -e "MINIO_SECRET_KEY=Meatwad01!" -d -v /home/snoyes/.minio:/root/.minio/ -v /mnt/data:/data minio/minio server --address ":443" /data
+    - create 2 buckets (foundation-installations, platform-automation)
 
-Add the "pa" properties into /concourse/pa in credhub
+Once bucc has configured the bosh/concourse vm:
+- add default creds to credhub unless they exist (./credhub-vars.sh)
+- create environment (./create_new_env.sh gcp new_gcp_environment_name dashaun.cloud)
+- Login in to concourse (bucc info to get password) 
+- add target to fly (fly --target example login --team-name my-team --concourse-url https://ci.example.com --insecure)
+- create team in concourse (fly -t example set-team --team-name my-team --local-user foo)
+- fly pipelines!
+
+sudo docker run -p 443:443 --name minio1 -e "MINIO_ACCESS_KEY=minio" -e "MINIO_SECRET_KEY=Meatwad01!" -d -v /home/snoyes/.minio:/root/.minio/ -v /mnt/data:/data minio/minio server --address ":443" /data
--- create_new_env.sh
+++ create_new_env.sh
@@ -10,13 +10,14 @@
 
 echo "IAAS = " $1
 echo "Environment Name = " $2
+echo "Domain Name = " $3
 
-if [ $# != 2 ]; then
-  echo "This requires 2 arguments i.e : create_new_env.sh gcp new_gcp_environment_name"
+if [ $# != 3 ]; then
+  echo "This requires 3 arguments i.e : create_new_env.sh gcp new_gcp_environment_name dashaun.cloud"
   exit 1
 fi
 
-echo "Adding $2 environment in $1 cloud"
+echo "Adding $2 environment in $1 cloud for $3 domain"
 credhub set -n /concourse/$2/iaas -t value -v $1
 credhub set -n /concourse/$2/environment_name -t value -v $2
 credhub set -n /concourse/$2/email -t value -v "$(credhub get -n /concourse/pa/email -j | jq .value)"
@@ -35,7 +36,7 @@ credhub set -n /concourse/$2/credhub_username -t value -v "$(credhub get -n /con
 credhub set -n /concourse/$2/credhub_password -t value -v "$(credhub get -n /concourse/main/credhub_password -j | jq .value)"
 
 echo "Adding git ssh key"
-credhub set -n /concourse/$2/git_ssh_key -t value -v "$(credhub get -n /concourse/pa/git_ssh_key -j | jq .value)"
+credhub set -n /concourse/$2/git_ssh_key -t value -v "$(credhub get -n /concourse/pa/git_ssh_key -j | jq .value.private_key)"
 
 echo "Adding OpsManager credentials"
 credhub set -n /concourse/$2/opsman_admin_username -t value -v admin
@@ -70,6 +71,28 @@ if [ "$1" = "azure" ]; then
   credhub set -n /concourse/$2/azure_terraform_storage_account_name -t value -v "$(credhub get -n /concourse/pa/azure_terraform_storage_account_name -j | jq .value)"
 fi
 
+if [ "$1" = "vsphere"]; then
+  echo "Adding VSphere vals"
+  credhub set --type value --name /concourse/$2/homelab_dns --value "$(credhub get -n /concourse/pa/homelab_dns -j | jq .value)"
+  credhub set --type value --name /concourse/$2/homelab_netmask --value "$(credhub get -n /concourse/pa/homelab_netmask -j | jq .value)"
+  credhub set --type value --name /concourse/$2/homelab_gateway --value "$(credhub get -n /concourse/pa/homelab_gateway -j | jq .value)"
+  credhub set --type value --name /concourse/$2/opsman_private_ip --value "$(credhub get -n /concourse/pa/opsman_private_ip -j | jq .value)"
+  credhub set --type value --name /concourse/$2/minio_endpoint --value "$(credhub get -n /concourse/pa/minio_endpoint -j | jq .value)"
+  credhub set --type value --name /concourse/$2/minio_secret_key --value "$(credhub get -n /concourse/pa/minio_secret_key -j | jq .value)"
+  credhub set --type value --name /concourse/$2/minio_access_key --value "$(credhub get -n /concourse/pa/minio_access_key -j | jq .value)"
+  credhub set --type value --name /concourse/$2/homelab_ssh_pub  --value "$(credhub get -n /concourse/pa/homelab_ssh_pub -j | jq .value.public_key)"
+  credhub set --type value --name /concourse/$2/homelab_network --value "$(credhub get -n /concourse/pa/homelab_network -j | jq .value)"
+  credhub set --type value --name /concourse/$2/vcenter_folder --value "$(credhub get -n /concourse/pa/vcenter_folder -j | jq .value)"
+  credhub set --type value --name /concourse/$2/vcenter_host --value "$(credhub get -n /concourse/pa/vcenter_host -j | jq .value)"
+  credhub set --type value --name /concourse/$2/vcenter_url --value "$(credhub get -n /concourse/pa/vcenter_url -j | jq .value)"
+  credhub set --type value --name /concourse/$2/vcenter_user --value "$(credhub get -n /concourse/pa/vcenter_user -j | jq .value)"
+  credhub set --type value --name /concourse/$2/vcenter_datastore --value "$(credhub get -n /concourse/pa/vcenter_datastore -j | jq .value)"
+  credhub set --type value --name /concourse/$2/vcenter_dc --value "$(credhub get -n /concourse/pa/vcenter_dc -j | jq .value)"
+  credhub set --type value --name /concourse/$2/opsman_hostname --value "$(credhub get -n /concourse/pa/opsman_hostname -j | jq .value)"
+  credhub set --type value --name /concourse/$2/opsman_vm_name --value "$(credhub get -n /concourse/pa/opsman_vm_name -j | jq .value)"
+  credhub set --type value --name /concourse/$2/vcenter_rp --value "$(credhub get -n /concourse/pa/vcenter_rp -j | jq .value)"
+fi 
+
 echo "Right now we are assuming that your domain name is managed by CloudFlare"
 
 # Create temporary cloudflare.ini file
@@ -83,13 +106,13 @@ sudo certbot certonly --dns-cloudflare \
                       --dns-cloudflare-propagation-seconds 60  \
                       --dns-cloudflare-credentials ./cloudflare.ini \
                       --preferred-challenges dns-01 \
-                      -d $2.$1.dashaun.cloud \
-                      -d *.apps.$2.$1.dashaun.cloud \
-                      -d *.sys.$2.$1.dashaun.cloud \
-                      -d opsmanager.$2.$1.dashaun.cloud \
-                      -d *.pks.$2.$1.dashaun.cloud \
+                      -d $2.$1.$3 \
+                      -d *.apps.$2.$1.$3 \
+                      -d *.sys.$2.$1.$3 \
+                      -d opsmanager.$2.$1.$3 \
+                      -d *.pks.$2.$1.$3 \
                       --agree-tos \
-                      -m dacarter@pivotal.io \
+                      -m "$(credhub get -n /concourse/pa/email -j | jq .value)" \
                       --cert-path ./
 
 echo "Certs exist"
@@ -99,8 +122,8 @@ credhub delete -n /concourse/$2/acme_cert
 # Store the certificate bits in credhub
 credhub set -n /concourse/$2/acme_cert \
             -t certificate \
-            -c "$(sudo cat /etc/letsencrypt/live/$2.$1.dashaun.cloud/fullchain.pem)" \
-            -p "$(sudo cat /etc/letsencrypt/live/$2.$1.dashaun.cloud/privkey.pem)"
+            -c "$(sudo cat /etc/letsencrypt/live/$2.$1.$3/fullchain.pem)" \
+            -p "$(sudo cat /etc/letsencrypt/live/$2.$1.$3/privkey.pem)"
 
 echo "If there were no DNS timeout errors, the certs were created/updated."
 echo "If there were timeouts, its okay to re-run this script multiple times, its idempotent"
