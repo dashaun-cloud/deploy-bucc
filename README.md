119c018c47f97f3e3dea9d2d0e560e88780ab640
Sean Noyes <sean@quickcadence.com>
2020-03-12 20:47:21 -0500

    Added additional scripts to handle credhub and updated README (#2)
    * Updating to allow vsphere and automate credhub properties
    
    * updating README
    
    * Changed ssh type for github key
    
    * Changed ssh type for github key
    
    * Changed ssh type for github key
    
    * Removing tags from readme
    
    * Changed ssh type for github key
    
    * Adding better line delimiters for readme
    
    * Adding sources for dependencies

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
 # deploy-bucc
 
-Make sure to grab a release and not master
+Make sure to grab a release and not master from https://github.com/starkandwayne/bucc
 
 For GCP: bucc up --cpi gcp --concourse-lb
+- Add a tag so we can create some firewall rules around it.  I've used "bucc" in the sample vars file.
 
-Add a tag so we can create some firewall rules around it.  I've used "bucc" in the sample vars file.
+Additional installation:
+- jq - apt install jq
+- ytt - https://github.com/k14s/ytt
+- certbot
+	- sudo apt update
+	- sudo apt install software-properties-common
+	- sudo add-apt-repository universe
+	- sudo add-apt-repository ppa:certbot/certbot
+	- sudo apt install certbot python3-certbot-dns-cloudflare -y
+- docker
+- yaml-patch - https://github.com/krishicks/yaml-patch/releases
+- minio (for on-prem/vsphere - create cert, run as https)
+	- sudo certbot certonly --dns-cloudflare \
+       --dns-cloudflare-propagation-seconds 60  \
+       --dns-cloudflare-credentials ./cloudflare.ini \
+       --preferred-challenges dns-01 \
+       -d minio.domain \
+       --agree-tos \
+       -m email
 
-Add the "pa" properties into /concourse/pa in credhub
+    - sudo docker run -p 443:443 --name minio1 -e "MINIO_ACCESS_KEY=key" -e "MINIO_SECRET_KEY=password" -d -v /home/snoyes/.minio:/root/.minio/ -v /mnt/data:/data minio/minio server --address ":443" /data
+      - create 2 buckets (foundation-installations, platform-automation)
+
+Once bucc has configured the bosh/concourse vm:
+- add default creds to credhub unless they exist (./credhub-vars.sh)
+- create environment (./create_new_env.sh gcp new_gcp_environment_name dashaun.cloud)
+- Login in to concourse (bucc info to get password) 
+- add target to fly (fly --target example login --team-name my-team --concourse-url https://ci.example.com --insecure)
+- create team in concourse (fly -t example set-team --team-name my-team --local-user foo)
+- fly pipelines!
---
+++ credhub-vars.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Modify/comment out sections which are not needed for your project
+# Everything else is required
+
+#General values (required) - we assume sendgrid and cloudflare for homelabs
+credhub set --type value --name /concourse/pa/wavefront-token --value <API token>
+credhub set --type value --name /concourse/pa/sendgrid-api --value <API token>
+credhub set --type value --name /concourse/pa/pivnet_token --value <API token>
+credhub set --type ssh --name /concourse/pa/git_ssh_key --public <path to public key> --private <path to private key>
+credhub set --type value --name /concourse/pa/cloudflare_email --value <account email>
+credhub set --type value --name /concourse/pa/cloudflare_key --value <API token>
+credhub set --type value --name /concourse/pa/email --value <notification email>
+credhub set --type value --name /concourse/pa/app_terraform_io_token --value <API token>
+credhub set --type value --name /concourse/pa/app_terraform_io_org --value <terraform org>
+
+#AWS specific values (comment if not needed)
+credhub set --type value --name /concourse/pa/aws_secret_key --value <aws secret>
+credhub set --type value --name /concourse/pa/aws_access_key --value <aws key>
+
+#Azure specific values (comment if not needed)
+credhub set --type value --name /concourse/pa/azure_cf_resource_group_name --value <resource group>
+credhub set --type value --name /concourse/pa/azure_cf_storage_account_name --value <storage account>
+credhub set --type value --name /concourse/pa/azure_subscription_id --value <subscription id>
+credhub set --type value --name /concourse/pa/azure_client_secret --value <client secret>
+credhub set --type value --name /concourse/pa/azure_client_id --value <client id>
+credhub set --type value --name /concourse/pa/azure_tenant_id --value <tenant id>
+credhub set --type value --name /concourse/pa/azure_terraform_container_name --value <tf container name> 
+credhub set --type value --name /concourse/pa/azure_terraform_resource_group_name --value <tf resource group>
+credhub set --type value --name /concourse/pa/azure_terraform_storage_account_name --value <tf storage account>
+credhub set --type value --name /concourse/pa/azure_access_key --value <access key>
+
+#GCP specific values (comment if not needed)
+credhub set --type value --name /concourse/pa/gcp_credentials_json --value <'<gcp json payload>'>
+credhub set --type value --name /concourse/pa/project --value <project name in gcp>
+
+# Values specific to VSphere on-prem environments (comment if not needed)
+credhub set --type value --name /concourse/pa/homelab_dns --value <dns address>
+credhub set --type value --name /concourse/pa/homelab_netmask --value <netmask>
+credhub set --type value --name /concourse/pa/homelab_gateway --value <gateway address>
+credhub set --type value --name /concourse/pa/opsman_private_ip --value <internal opsman ip>
+credhub set --type value --name /concourse/pa/minio_endpoint --value <minio endpoint>
+credhub set --type value --name /concourse/pa/minio_secret_key --value <secret key>
+credhub set --type value --name /concourse/pa/minio_access_key --value <access key>
+credhub set --type ssh --name /concourse/pa/homelab_ssh_pub  --public <path to public key> --private <path to private key>
+credhub set --type value --name /concourse/pa/homelab_network --value <network name in vsphere>
+credhub set --type value --name /concourse/pa/vcenter_folder --value <vcenter folder name>
+credhub set --type value --name /concourse/pa/vcenter_host --value <vcenter hostname>
+credhub set --type value --name /concourse/pa/vcenter_url --value <vcenter url (https://hostname)>
+credhub set --type value --name /concourse/pa/vcenter_user --value <full sso user name (administrator@domain)>
+credhub set --type value --name /concourse/pa/vcenter_datastore --value <vcenter datastore>
+credhub set --type value --name /concourse/pa/vcenter_dc --value <vcenter datacenter name>
+credhub set --type value --name /concourse/pa/opsman_hostname --value <hostname for opsman>
+credhub set --type value --name /concourse/pa/opsman_vm_name --value <vm name for opsman>
+credhub set --type value --name /concourse/pa/vcenter_rp --value <vcenter resource pool>
+
